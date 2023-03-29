73c4816ef141a717178b3707fe2d2f63dc7d0acd
taufiqqq <88092798+taufiqqq@users.noreply.github.com>
2023-03-29 11:51:24 +0800

    Update Course.java
    29/3 OOP lecture on class method

--- CourseRegApp/src/Course.java
+++ CourseRegApp/src/Course.java
@@ -3,20 +3,25 @@ public class Course {
     private String name;
     private String courseCode;
     private int credit;
-    private Student listOfStudent[] = new Student[30];
-    private int studentNum = 0;
+    private Student listOfStudent[];
+    private int studentNum;
+    final int MAX = 30;
     private Lecturer lec;
 
-    Course() {
+    public Course() {
         name = "";
         courseCode = "";
         credit = 0;
+        listOfStudent = new Student[MAX];
+        studentNum = 0;
     }
 
-    Course(String name, String courseCode, int credit) {
+    public Course(String name, String courseCode, int credit) {
         this.name = name;
         this.courseCode = courseCode;
         this.credit = credit;
+        listOfStudent = new Student[MAX];
+        this.studentNum = 0;
     }
 
     public void setName(String name) {
@@ -50,29 +55,39 @@ public class Course {
     }
 
     public void addStudent(Student s) {
-        if (studentNum < 30) {
-            listOfStudent[studentNum] = s;
-            studentNum++;
-        }
-        else {
+        int status = 0; // 0 if the student hasnt registered, 1/any other number if already registered
+
+        if (studentNum < MAX) {
+            for (int i = 0; i < studentNum; i++) {
+                if (s.getMatNum() == listOfStudent[i].getMatNum())
+                    status = 1; // indicates that student already registered. therefore wont be adding the
+                                // student into the array
+            }
+            if (status == 0)
+            {
+                listOfStudent[studentNum] = s;
+                studentNum++;
+            }
+        } else
             System.out.println("FULL");
-        }
+
     }
 
     public void addLecturer(Lecturer l) {
         if (lec == null) {
             lec = l;
-        }
-        else {
+        } else {
             System.out.println("Lecturer already assigned");
         }
     }
 
-    public void printStudents(){
-        for(int i = 0; i<30; i++){
-            if(listOfStudent[i] != null)
-                System.out.println(listOfStudent[i].getName());
-            else break;
+    public void printStudents() {
+        System.out.println("\n=== STUDENT FROM CLASS " + this.name.toUpperCase() + " ===");
+        for (int i = 0; i < MAX; i++) {
+            if (listOfStudent[i] != null)
+                System.out.println(i + 1 + ". " + listOfStudent[i].getName());
+            else
+                break;
         }
     }
 
