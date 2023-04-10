5fc7084015e399a33f25ca56fe4449bba9ce23fd
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2023-04-10 09:18:27 +0800

    Update App.java
    Obtain data from array

--- ArrayApp/src/App.java
+++ ArrayApp/src/App.java
@@ -12,6 +12,8 @@ public class App {
         char[] aChar = { 'A', 'B', 'C', 'D' };
         // or char aChar[] = {'A', 'B', 'C', 'D'};
 
+        float fNum[] = { 34.5f, 12.5f, 11.2f, 15.0f, -10.5f, 101f, -1f };
+
         for (int i = 0; i < aNum.length; i++) {
             System.out.println("Value of int [" + i + "] : " + aNum[i]);
         }
@@ -25,5 +27,28 @@ public class App {
             System.out.println("Value of char " + i + ": " + aChar[i]);
             ++i;
         }
+
+        float largestElement = fNum[0];
+        float sum = 0;
+        int smallestIndex = 0;
+        float smallestElement = fNum[0];
+
+        for (int j = 0; j < fNum.length; j++) {
+            System.out.println("List of float [" + j + "] : " + fNum[j]);
+            
+            sum = sum + fNum[j];
+            
+            if (fNum[j] > largestElement)
+                largestElement = fNum[j];
+            
+            if (fNum[j] < smallestElement) {
+                smallestIndex = j;
+                smallestElement = fNum[j];
+            }
+        }
+
+        System.out.println("Sum of Array: " + sum);
+        System.out.println("Largest Element: " + largestElement);
+        System.out.println("Smallest Index: " + smallestIndex + " with element of " + smallestElement);
     }
 }
